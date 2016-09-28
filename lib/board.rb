9b6803ee22b4e1d8865d1331ab0fa84ba66c1ce1
Martin Reiche <reiche.stud@gmail.com>
2016-09-28 09:11:38 +0200

    Finished Field and started board specs

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,26 @@
 # Add documentaion
-class board
-  
+class Board
+  attr_reader :fields
+  def initialize
+    @fields = []
+    8.times.each do |row|
+      8.times.each do |col|
+        @fields << Field.new([row,col])
+      end
+    end
+  end
 end
 
-class field
-  
+# Field of chess board. Stores corrdinates and a reference to the current
+# figure on the field.
+class Field
+  attr_reader :row, :col, :coord, :figure
+  def initialize(coordinates)
+    @@arg_err.call if (coordinates.class != Array) or (coordinates.length != 2) 
+    @coord = coordinates
+    @row = coord[0]
+    @col = coord[1]
+  end
+  #Errors
+  @@arg_err = Proc.new { raise ArgumentError.new("Argument must be an object of the Node class") }
 end
--- spec/lib/board_spec.rb
+++ spec/lib/board_spec.rb
@@ -1,6 +1,48 @@
 require 'spec_helper'
 require 'board'
 
-describe board do
-  
+describe Board do
+  it { is_expected.to respond_to(:fields) }
+
+  describe '.coords' do
+
+  end
+  describe '.rows' do
+
+  end
+  describe '.cols' do
+
+  end
+  describe '.figures' do
+
+  end
+end
+
+
+describe Field do
+  subject { Field.new([0,0]) }
+  it { is_expected.to respond_to(:row) }
+  it { is_expected.to respond_to(:col) }
+  it { is_expected.to respond_to(:coord) }
+  it { is_expected.to respond_to(:figure) }
+  describe '.new' do
+    before(:all) { @field = Field.new([2,1]) }
+    it "should be initialized with coordinates" do
+      expect(@field.coord).to eq([2,1])
+    end
+    it "row should be the first of coordinates" do
+      expect(@field.row).to eq(@field.coord[0])
+    end
+    it "col should be the second of coordinates" do
+      expect(@field.col).to eq(@field.coord[1])
+    end
+    it "should be initailized with as empty field" do
+      expect(@field.figure).to be nil
+    end
+    it "raises an error when argument is not a an array" do
+      expect {Field.new([1])}.to raise_error(ArgumentError)
+      expect {Field.new([1,2,3])}.to raise_error(ArgumentError)
+      expect {Field.new()}.to raise_error(ArgumentError)
+    end
+  end
 end
