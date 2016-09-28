720c068cf4a08ba10fc2909b71ac8dcfc3211724
Martin Reiche <reiche.stud@gmail.com>
2016-09-28 10:42:49 +0200

    Before Player branch

--- lib/figures.rb
+++ lib/figures.rb
@@ -1,4 +1,10 @@
 # Add docu
 class Figure
+  attr_reader :name, :player, :player_id, :type
+
+  
+end
+
+class Knight < Figure
 
 end
--- spec/lib/figures_spec.rb
+++ spec/lib/figures_spec.rb
@@ -2,5 +2,14 @@ require 'spec_helper'
 require 'figures'
 
 describe Figure do
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:player) }
+  it { is_expected.to respond_to(:player_id) }
+  it { is_expected.to respond_to(:type) }
+  
+  
+end
 
+describe Knight do
+  it { expect(Knight).to be < Figure }
 end
